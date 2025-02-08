e85ffc9ee5abdc499f8613ce95d33d8423583597
Bruni <bruno.a.dinis96@gmail.com>
2025-02-08 13:54:17 -0300

    Criando o Hero e importando a font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')(require("sass"))
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require('gulp-imagemin')
 
-function styles(){
-  console.log("Compilando o sass")
-  return gulp.src('./src/styles/*.scss')
-  .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-  .pipe(gulp.dest('./dist/src/css'))
+function styles() {
+  console.log("Compilando o sass");
+  return gulp
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./dist/src/css"));
 }
 
-exports.default = styles;
-exports.watch = ()=>{
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-
+// Comprimindo as imagens
+function images() {
+  return gulp
+    .src("./src/images/**/*", { encoding: false })
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
 }
+
+exports.default = gulp.parallel(styles, images);
+exports.watch = () => {
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+};
