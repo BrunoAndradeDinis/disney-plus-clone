7943ca63252b267e939e22de551158e9ecadc8cc
Bruni <bruno.a.dinis96@gmail.com>
2025-02-13 21:20:25 -0300

    feat: add section.faq - update no html, scss e js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // pegando os buttons com o data pre-definido
   const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
 
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (button) => {
@@ -9,10 +10,22 @@ document.addEventListener("DOMContentLoaded", () => {
       hideAllTabs();
       hideAllButtonsActive();
       tab.classList.add("shows__list--is-active");
-      button.target.classList.add("shows__tabs__button--is-active")
+      button.target.classList.add("shows__tabs__button--is-active");
     });
   }
+
+  // evento de clique nas questions da faq
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", abreOuFecharesposta);
+  }
 });
+
+function abreOuFecharesposta(e) {
+  const classe = "faq__questions__item--is-open";
+  const elementoPai = e.target.parentElement;
+
+  elementoPai.classList.toggle("faq__questions__item--is-open");
+}
 
 function hideAllTabs() {
   // pegando as uls com o data pre-definido
